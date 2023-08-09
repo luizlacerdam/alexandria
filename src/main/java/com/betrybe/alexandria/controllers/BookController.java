de5598700352dd3b1b7d8634aa5cd4f4eeb21fb4
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-08-08 22:01:16 -0300

    BookController-createBook

--- src/main/java/com/betrybe/alexandria/controllers/BookController.java
+++ src/main/java/com/betrybe/alexandria/controllers/BookController.java
@@ -1,7 +1,14 @@
 package com.betrybe.alexandria.controllers;
 
+import com.betrybe.alexandria.controllers.dto.BookDTO;
+import com.betrybe.alexandria.controllers.dto.ResponseDTO;
+import com.betrybe.alexandria.models.entities.Book;
 import com.betrybe.alexandria.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,5 +21,12 @@ public class BookController {
   @Autowired
   public BookController(BookService bookService) {
     this.bookService = bookService;
+  }
+
+  @PostMapping()
+  public ResponseEntity<ResponseDTO<Book>> createBook(@RequestBody BookDTO bookDTO) {
+    Book newBook = bookService.insertBook(bookDTO.toBook());
+    ResponseDTO<Book> responseDTO = new ResponseDTO<>("Livro criado com sucesso!", newBook);
+    return ResponseEntity.status(HttpStatus.CREATED).body(responseDTO);
   }
 }
