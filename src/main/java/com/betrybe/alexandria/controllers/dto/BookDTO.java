4d52e0dc371e657a1819340d818d919772c2a8a2
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-08-08 21:59:19 -0300

    ResponseDTO

--- src/main/java/com/betrybe/alexandria/controllers/dto/BookDTO.java
+++ src/main/java/com/betrybe/alexandria/controllers/dto/BookDTO.java
@@ -1,5 +1,11 @@
 package com.betrybe.alexandria.controllers.dto;
 
-public class BookDTO {
+import com.betrybe.alexandria.models.entities.Book;
+
+public record BookDTO(Long id, String title, String genre) {
+
+  public Book toBook() {
+    return new Book(id, title, genre);
+  }
 
 }
