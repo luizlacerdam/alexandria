99d213746b8f9102e9e7b05b5627953a60a01412
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-08-08 22:01:42 -0300

    BookController-updateBook

--- src/main/java/com/betrybe/alexandria/controllers/BookController.java
+++ src/main/java/com/betrybe/alexandria/controllers/BookController.java
@@ -4,10 +4,13 @@ import com.betrybe.alexandria.controllers.dto.BookDTO;
 import com.betrybe.alexandria.controllers.dto.ResponseDTO;
 import com.betrybe.alexandria.models.entities.Book;
 import com.betrybe.alexandria.service.BookService;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,5 +31,20 @@ public class BookController {
     Book newBook = bookService.insertBook(bookDTO.toBook());
     ResponseDTO<Book> responseDTO = new ResponseDTO<>("Livro criado com sucesso!", newBook);
     return ResponseEntity.status(HttpStatus.CREATED).body(responseDTO);
+  }
+
+  @PutMapping("/{bookId}")
+  public ResponseEntity<ResponseDTO<Book>> updateBook(@PathVariable Long bookId, @RequestBody BookDTO bookDTO) {
+    Optional<Book> optionalBook = bookService.updateBook(bookId, bookDTO.toBook());
+
+    if (optionalBook.isEmpty()) {
+      ResponseDTO<Book> responseDTO = new ResponseDTO<>(
+          String.format("Não foi encontrado o livro de ID %d", bookId), null);
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body(responseDTO);
+    }
+
+    ResponseDTO<Book> responseDTO = new ResponseDTO<>(
+        "Livro atualizado com sucesso!", optionalBook.get());
+    return ResponseEntity.ok(responseDTO);
   }
 }
