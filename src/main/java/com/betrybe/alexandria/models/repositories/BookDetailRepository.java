035864a93b64706cf31b93b8bf6a3eef0313d23b
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-08-09 11:04:51 -0300

    BookDetailRepository

--- src/main/java/com/betrybe/alexandria/models/repositories/BookDetailRepository.java
+++ src/main/java/com/betrybe/alexandria/models/repositories/BookDetailRepository.java
@@ -2,7 +2,9 @@ package com.betrybe.alexandria.models.repositories;
 
 import com.betrybe.alexandria.models.entities.BookDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BookDetailRepository extends JpaRepository<BookDetail, Long> {
 
 }
